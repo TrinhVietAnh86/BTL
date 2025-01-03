afd08a5f129cce9f541a0a7287b97ddcbff607cc
thang-fox <lethang160705@gmail.com>
2025-01-03 16:20:11 +0700

    thêm danh mục sảnh phẩm thiếu xóa và sửa

---
+++ admin/modum/menu.php
@@ -0,0 +1,5 @@
+<ul class="admin">
+    <li><a href="index.php?action=quanlydanhmucsanpham">quản lý doanh mục</a></li>
+    <li><a href="index.php?action=quanlydanhsanpham">quản lý sản phẩm</a></li>
+    <li><a href="index.php?action=quanlybaiviet">quản lý bài viết</a></li>
+</ul>

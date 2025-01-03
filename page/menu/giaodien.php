c9c47f4766e2b788fd589ea1a8f0203c381a1f24
thang-fox <lethang160705@gmail.com>
2025-01-03 09:54:40 +0700

    sửa tên giao dien

--- page/menu/giaodien.php
+++ page/menu/giaodien.php
@@ -23,7 +23,7 @@
             <ul>
                 <li><a href="#">Trang Chủ</a></li>
                 <li><a href="#">Giới Thiệu</a></li>
-                <li><a href="#">Sản Phẩm</a></li>
+                <li><a href="#">Danh mục sản Phẩm</a></li>
                 <li><a href="#">Tin Tức</a></li>
                 <li><a href="#">Liên Hệ</a></li>
             </ul>
